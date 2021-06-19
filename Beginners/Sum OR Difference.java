a4e851d7fe449b3fdef75d4120c16e5035c90b74
Vicky Gupta <vickygupta9987@gmail.com>
2021-06-20 03:51:51 +0530

    https://github.com/viki4545/Codechef-Java-Solution.git

---
+++ Beginners/Sum OR Difference.java
@@ -0,0 +1,21 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int n1 = sc.nextInt();
+	    int n2 = sc.nextInt();
+	    if(n1 > n2){
+	        System.out.println(n1-n2);
+	    }else{
+	        System.out.println(n1+n2);
+	    }
+	}
+}
