ea814a17f22cb3959f67e46b8bdbcee92b2f1850
Vicky Gupta <vickygupta9987@gmail.com>
2021-06-26 22:45:36 +0530

    files uploaded

--- Beginners/Lapindromes.java
+++ Beginners/Lapindromes.java
@@ -6,35 +6,37 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes here
 		Scanner sc = new Scanner(System.in);
 		int testCases = sc.nextInt();
 		while(testCases --> 0){
+		    //we have to take the string object to take input as a character
 		    String str = new String();
 		    str = sc.next();
 		    int len = str.length();
 		    int[] left = new int[26];
 		    int[] right = new int[26];
-		    
+		    	
+			//we have taken left array to iterate through 0th to mid(len/2) index
 		        for(int i = 0;i<len/2;i++){
 		            left[str.charAt(i) - 'a']++;
 		        }
-		        
+
+		        //we have taken right array to iterate throught len+1/2(mid+1) to last index
 		        for(int i = (len+1)/2;i<len;i++){
 		            right[str.charAt(i) - 'a']++;
 		        }
-		        
+
 		        boolean flag = true;
+			//we iterate the array from left to right to check if left is not equal to 				//right then it will return false or it will return true
 		        for(int i = 0;i<26;i++){
 		            if(left[i] != right[i])
 		              flag = false;
 		        }
-		        
+
 		    if(flag == true)
 		        System.out.println("YES");
 		    else
--- Beginners/Turbo Sort.java
+++ Beginners/Turbo Sort.java
@@ -1,23 +1,29 @@
-/* package codechef; // don't place package name! */
 
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
 class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc = new Scanner(System.in);
+
 		int testCases = sc.nextInt();
-		int[] arr = new int[testCases];
-		for(int i = 0;i<arr.length;i++){
-		    arr[i] = sc.nextInt();
-		}
-		Arrays.sort(arr);
-		for(int i = 0;i<arr.length;i++){
-		    System.out.println(arr[i]);
-		}
+		// we have taken the array of size testCases
+		   int[] arr = new int[testCases];
+			//this loop will iterate from 0 to length of the array
+			//to take the user input as array index value
+			for(int i = 0;i<arr.length;i++){ 
+
+		    		arr[i] = sc.nextInt();
+			}
+			//this will sort the array in ascending order
+			Arrays.sort(arr);
+			//this loop will print the array in the ascending order
+			for(int i = 0;i<arr.length;i++){
+			    
+				System.out.println(arr[i]);
+			}
 	}
 }
